ba88367c99facec376ff2a9709cf5c349cbd0f86
swapnilgarg7 <swapnilgarg810@gmail.com>
2024-03-22 20:28:40 +0530

    fixattempt1

--- components/hero.js
+++ components/hero.js
@@ -32,13 +32,12 @@ const Hero = () => {
         <div className="flex items-center justify-center w-full lg:w-1/2">
           <div className="">
             <Image
-              src={heroImg}
+              src="/1.jpeg"
               width="616"
               height="617"
               className={"object-cover"}
               alt="Hero Illustration"
               loading="eager"
-              placeholder="blur"
             />
           </div>
         </div>
